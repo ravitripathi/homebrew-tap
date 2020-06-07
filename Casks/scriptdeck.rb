524d8eb4c5400ea819b7769fb26576291c4ab73f
Ravi Tripathi <ravitripathi1996@gmail.com>
2020-06-08 03:33:37 +0530

    RC1

--- Casks/scriptdeck.rb
+++ Casks/scriptdeck.rb
@@ -1,7 +1,7 @@
 cask 'scriptdeck' do
-  version 'beta2'
+  version 'rc1'
   sha256 :no_check
-  url 'https://github.com/ravitripathi/ScriptDeck/releases/download/beta2/ScriptDeck.zip'
+  url 'https://github.com/ravitripathi/ScriptDeck/releases/download/RC1/ScriptDeck.zip'
   appcast 'https://github.com/ravitripathi/ScriptDeck/releases.atom'
   name 'ScriptDeck'
   homepage 'https://github.com/ravitripathi/ScriptDeck'
