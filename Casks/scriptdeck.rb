70494680c1e657de3b8ded8691b6ea8876a7dd38
Ravi Tripathi <ravitripathi1996@gmail.com>
2020-06-15 01:42:34 +0530

    Bumped Version to "1.0.0"

--- Casks/scriptdeck.rb
+++ Casks/scriptdeck.rb
@@ -1,7 +1,7 @@
 cask 'scriptdeck' do
-  version "0.8.0"
+  version "1.0.0"
   sha256 :no_check
-  url "https://github.com/ravitripathi/ScriptDeck/releases/download/0.8.0/ScriptDeck.app.zip"
+  url "https://github.com/ravitripathi/ScriptDeck/releases/download/1.0.0/ScriptDeck.app.zip"
   appcast 'https://github.com/ravitripathi/ScriptDeck/releases.atom'
   name 'ScriptDeck'
   homepage 'https://github.com/ravitripathi/ScriptDeck'
